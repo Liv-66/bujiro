abb4d4c6309df4d8ff2d3de93ab5c41d8c97259a
Liv Huang <livhuang95@gmail.com>
2020-12-01 17:08:24 +0800

    add bootswatch

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-const express = require("express");
-const handlebars = require("express-handlebars");
-const session = require("express-session");
-const bodyParser = require("body-parser");
-const methodOverride = require("method-override");
+const express = require('express');
+const handlebars = require('express-handlebars');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+const routes = require('./routes');
 
 const PORT = 3000;
 
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
 }
 
 const app = express();
+
+app.engine('hbs', handlebars({ defaultLayout: 'main.hbs' }));
+app.set('view engine', 'hbs');
+
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`connect to http://localhost:${PORT}`);
