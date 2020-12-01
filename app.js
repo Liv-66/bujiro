4e8cb20579025adb2f1c919b943ef2496f6f9ca7
Liv Huang <livhuang95@gmail.com>
2020-12-01 16:07:37 +0800

    project init

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const handlebars = require("express-handlebars");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+
+const PORT = 3000;
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`connect to http://localhost:${PORT}`);
+});
