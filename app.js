a665726f36dde700b1241c79cd158a4d48ee85ff
Liv Huang <livhuang95@gmail.com>
2020-12-06 17:28:19 +0800

    heroku pory

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const flash = require('connect-flash');
 const routes = require('./routes');
 const passport = require('./config/passport');
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
