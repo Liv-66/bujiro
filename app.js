3154930660f40ec94a35f2b23bf2da134db57e8e
Liv Huang <livhuang95@gmail.com>
2020-12-07 19:35:47 +0800

    bujiro project

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
-const flash = require('connect-flash');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const flash = require("connect-flash");
+const path = require('path')
 
-const routes = require('./routes');
-const passport = require('./config/passport');
+const routes = require("./routes");
+const passport = require("./config/passport");
 
 const PORT = process.env.PORT || 3000;
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
 const app = express();
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
-app.set('view engine', 'hbs');
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
 
-app.use(express.json({ limit: '10kb' }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json({ limit: "10kb" }));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 app.use(
   session({
-    secret: 'expense-tracher-session-secret',
+    secret: "expense-tracher-session-secret",
     resave: false,
     saveUninitialized: true,
   })
@@ -36,9 +39,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use((req, res, next) => {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.warning_msg = req.flash('warning_msg');
-  res.locals.delete_msg = req.flash('delete_msg');
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.warning_msg = req.flash("warning_msg");
+  res.locals.delete_msg = req.flash("delete_msg");
   res.locals.user = req.user;
   res.locals.isAuthenticated = req.isAuthenticated();
   next();
